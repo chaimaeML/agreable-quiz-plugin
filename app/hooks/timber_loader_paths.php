4527672db5e343c45fdaeb9c750b09982bc50134
garethfoote <gareth.foote@gmail.com>
2015-06-16 15:32:52 +0100

    Commenting

--- app/hooks/timber_loader_paths.php
+++ app/hooks/timber_loader_paths.php
@@ -4,18 +4,15 @@
 
 use SLM_QuizPlugin\Helper;
 
+/*
+ * We want Timber to render our plugin templates so we need to
+ * add our views onto the $paths array that Timber uses internally.
+ * We do this using a Wordpress filter that Timber instantiates:
+// @see https://github.com/jarednova/timber/blob/master/lib/timber-loader.php#L225
+ */
+
 // Called when Timber gets Twig loader.
 add_filter('timber/loader/paths', 'addPaths');
-// @see https://github.com/jarednova/timber/blob/master/lib/timber-loader.php#L225
-
-// Just before the file is rendered. Can bypass file selection here.
-// add_filter('timber_render_file', 'add_paths');
-// @see https://github.com/jarednova/timber/blob/master/timber.php#L285
-
-// Passes an instance of Twig just before rendering but after tpl selection.
-// add_filter('twig_apply_filters', 'update_twig_loader');
-// @see https://github.com/jarednova/timber/blob/master/timber.php#L307
-
 
 /*
  * use with Timber filter 'timber/loader/paths' to add Herbert views to array
@@ -33,3 +30,17 @@ function addPaths($paths){
   }
   return $paths;
 }
+
+// Other possible Timber filters below were used in attempts to access
+// the TwigEnvironment object so we could add Twig namespaces. No luck.
+// Leaving these here for the moment because they might be useful.
+
+// Just before the file is rendered. Can bypass file selection here.
+// add_filter('timber_render_file', 'add_paths');
+// @see https://github.com/jarednova/timber/blob/master/timber.php#L285
+
+// Passes an instance of Twig just before rendering but after tpl selection.
+// add_filter('twig_apply_filters', 'update_twig_loader');
+// @see https://github.com/jarednova/timber/blob/master/timber.php#L307
+
+
