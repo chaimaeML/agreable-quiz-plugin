14543d0053ce268ddc2057ba6f39f8a6575a72e5
garethfoote <gareth.foote@gmail.com>
2015-06-16 10:24:35 +0100

    Fixing panel controller and removing has from colour selection

--- app/Controllers/PanelController.php
+++ app/Controllers/PanelController.php
@@ -7,22 +7,28 @@ class PanelController {
     public function settings(Http $http){
 
         if ( $http->has('quiz_primary_colour') ){
-            $primary_colour = $this->plugin->http->get('quiz_primary_colour');
+            $primary_colour = $http->get('quiz_primary_colour');
+            if(substr($primary_colour, 0, 1) === '#'){
+              $primary_colour = substr($primary_colour, 1);
+            }
             update_option('quiz_primary_colour', $primary_colour);
         }
 
         if ( $http->has('quiz_secondary_colour') ){
-            $secondary_colour = $this->plugin->http->get('quiz_secondary_colour');
+            $secondary_colour = $http->get('quiz_secondary_colour');
+            if(substr($secondary_colour, 0, 1) === '#'){
+              $secondary_colour = substr($secondary_colour, 1);
+            }
             update_option('quiz_secondary_colour', $secondary_colour);
         }
 
         if ( $http->has('quiz_override_css') ){
-            $override_css = $this->plugin->http->get('quiz_override_css');
+            $override_css = $http->get('quiz_override_css');
             update_option('quiz_override_css', $override_css);
         }
 
         if ( $http->has('quiz_font_family') ){
-            $font_family = $this->plugin->http->get('quiz_font_family');
+            $font_family = $http->get('quiz_font_family');
             update_option('quiz_font_family', $font_family);
         }
 
