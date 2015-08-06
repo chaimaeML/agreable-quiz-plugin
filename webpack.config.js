d151639387cd667701a5bee98d673361f424eb18
garethfoote <gareth.foote@gmail.com>
2015-08-06 17:24:17 +0100

    Separated quiz into separate module. Updated webpack to uglify JS.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,30 @@
 var nib = require('nib')
 var path = require('path')
+var webpack = require('webpack')
 var ExtractTextPlugin = require("extract-text-webpack-plugin")
 
 var buildPath = path.resolve(__dirname, 'resources', 'assets');
 var mainPath = path.resolve(__dirname, 'src', 'main.js');
 
 module.exports = {
-  entry: mainPath,
+  entry: {
+    app     : mainPath
+  },
   output: {
     path: buildPath,
-    filename: 'app.js'
+    filename: '[name].js'
   },
   module: {
     loaders: [
       { test: /\.styl$/, loader: ExtractTextPlugin.extract("style", "css!stylus")},
       { test: /\.js$/, exclude:'node_modules', loader: 'babel' },
+      { test: /backbone/, exclude:'node_modules', loader: "imports?define=>false&this=>window" }
     ]
   },
 
   plugins: [
-    new ExtractTextPlugin('styles.css')
+    new ExtractTextPlugin('styles.css'),
+    new webpack.optimize.UglifyJsPlugin()
   ],
 
   resolve: {
